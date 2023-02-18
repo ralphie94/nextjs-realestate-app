d88a5cbd80cf4ee1807058fa350342d872f4e1dd
Ralphie Celedon <ralphie94@pacbell.net>
2023-02-17 23:32:49 -0800

    started next js app

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Flex, Box, Tex, Button } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <>
+      Hello
+    </>
+  )
+}
